600f2b76a29bb827a4b24363dcde7482cf1cb232
x215617 <X215617@prservicos.com.br>
2020-06-27 14:13:31 -0300

    coisas

--- src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,13 +1,17 @@
 package guru.springframework.sfgpetclinic;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SfgPetClinicApplicationTests {
 
 	@Test
 	void contextLoads() {
+		//no need for code here
 	}
 
 }
